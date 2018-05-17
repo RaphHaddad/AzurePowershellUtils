6c4ab8983865f7166e16012493fc47c67c53c7b6
Raphael <raphael@haddad.id.au>
2018-05-17 15:18:21 +1000

    Add CreateOrUpdate-KeyVaultSecret and rename resources

---
+++ CreateOrUpdate-KeyVaultSecret.ps1
@@ -0,0 +1,21 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [String]
+    $Subscription,
+
+    [Parameter(Mandatory=$true)]
+    [String]
+    $VaultName,
+
+    [Parameter(Mandatory=$true)]
+    [String]
+    $SecretName,
+
+    [Parameter(Mandatory=$true)]
+    [String]
+    $PlainTextSecret
+)
+
+$secret = = ConvertTo-SecureString -String $PlainTextSecret -AsPlainText -Force
+Select-AzureRmSubscription -Subscription $Subscription
+Set-AzureKeyVaultSecret -VaultName $VaultName -Name $SecretName -SecretValue $Secret
