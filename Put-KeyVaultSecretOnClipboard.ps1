670383e3984454233665cfc3ee141b10ed5171b8
Raphael <raphael@haddad.id.au>
2018-05-25 15:59:05 +1000

    Add script to put KeyVaultSecretOnClipboard

---
+++ Put-KeyVaultSecretOnClipboard.ps1
@@ -0,0 +1,29 @@
+param(
+    [Parameter(Mandatory=$true)]
+    [String]
+    $Subscription,
+
+    [Parameter(Mandatory=$true)]
+    [String]
+    $VaultName,
+
+    [Parameter(Mandatory=$true)]
+    [String]
+    $SecretName,
+
+
+    [Parameter()]
+    [switch]
+    $Show
+)
+
+$null = Select-AzureRmSubscription -Subscription $Subscription
+
+$secret = Get-AzureKeyVaultSecret -VaultName $VaultName -Name $SecretName
+
+if ($Show) {
+    Write-Host $secret.SecretValueText -ForegroundColor Green
+}
+else {
+    $secret.SecretValueText | clip
+}
